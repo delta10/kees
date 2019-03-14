d16b7a3c359bd596c86050d71b29f7bacacb85cb
Bart Jeukendrup <bart@jeukendrup.nl>
2019-03-14 13:27:39 +0100

    Added translations to filter

--- core/filters.py
+++ core/filters.py
@@ -3,8 +3,15 @@ from .models import Case
 
 
 class CaseFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains')
+    name = django_filters.CharFilter(lookup_expr='icontains', label='Naam')
 
     class Meta:
         model = Case
-        fields = ['name', 'case_type', 'current_phase', 'created_at', 'assignee']
+        fields = ['name', 'case_type', 'current_phase', 'assignee']
+
+    def __init__(self, *args, **kwargs):
+        super(CaseFilter, self).__init__(*args, **kwargs)
+
+        self.filters['case_type'].label = 'Zaaktype'
+        self.filters['current_phase'].label = 'Huidige fase'
+        self.filters['assignee'].label = 'Behandelaar'
