b748c58b674c9f72836004be9deab06e31d22782
Bart Jeukendrup <bart@jeukendrup.nl>
2020-05-25 17:32:11 +0200

    feat(): add ability to change password

--- kees/urls.py
+++ kees/urls.py
@@ -61,6 +61,8 @@ urlpatterns = [
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('change-password/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('change-password/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('admin/', admin.site.urls),
 ]
 
