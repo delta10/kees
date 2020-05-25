9e82b843ac4e53ed6d3d48fe47c97f9c8a569f70
Bart Jeukendrup <bart@jeukendrup.nl>
2020-05-25 12:37:39 +0200

    feat(): add ability to reset password

--- kees/settings.py
+++ kees/settings.py
@@ -127,16 +127,10 @@ WSGI_APPLICATION = 'kees.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'OPTIONS': {
+            'min_length': 7,
+        }
     },
 ]
 
@@ -199,5 +193,6 @@ LOGIN_REDIRECT_URL = 'dashboard'
 
 LOGIN_EXEMPT_URLS = [
     '^api/(.+)$',
-    '^media/(.+)$'
+    '^media/(.+)$',
+    '^accounts/(.+)$'
 ]
