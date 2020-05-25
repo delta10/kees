dc6b3d17e795f65ac29f5c35cdcd01ebdd9b938e
Bart Jeukendrup <bart@jeukendrup.nl>
2020-05-25 12:47:14 +0200

    fix(): handle case when sumkey value is None

--- core/templatetags/sumkey.py
+++ core/templatetags/sumkey.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.filter(name='sumkey')
 def sumkey(values, key):
-    return sum([int(item.get(key, 0)) for item in values])
+    return sum([(int(item.get(key)) if item.get(key) else 0) for item in values])
